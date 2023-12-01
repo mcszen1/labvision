a1b5951e6b3a510a0b9f1f90f0d19d53ed02a603
mcszen1 <mcszen@gmail.com>
2023-12-01 19:04:02 -0300

    Update Ingredientes.py

--- Ingredientes.py
+++ Ingredientes.py
@@ -87,10 +87,9 @@ def main():
         
                 # Analyze the image with OpenAI's GPT-4 Vision API
                 description = analyze_image_with_openai(image_url)
+		total_calorias = calcular_calorias_do_prato(description, tabela_calorias)
 		st.write("Description: ")
 		st.write(description)
-                # Display the description and translated text
-                total_calorias = calcular_calorias_do_prato(description, tabela_calorias)
 		st.write(f"Calorias totais do prato: {total_calorias}")
     if option=='Camera':
         picture = st.camera_input("Tire uma foto")
