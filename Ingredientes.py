15d41dc7f01d0e34bb44d2ff35f1fe74af405545
mcszen1 <mcszen@gmail.com>
2023-12-01 19:41:46 -0300

    Update Ingredientes.py

--- Ingredientes.py
+++ Ingredientes.py
@@ -4,24 +4,22 @@ import pyttsx3
 import os
 import tempfile
 import requests, uuid, json
+import azure.cognitiveservices.speech as speechsdk
 from openai import OpenAI
 import json
-import streamlit as st
-import pandas as pd
-from fuzzywuzzy import process
 
 # Initialize the OpenAI client with your API key
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-description=""
+
 def save_uploaded_file(uploaded_file_content):
     with open("temp_image.jpg", "wb") as f:
         f.write(uploaded_file_content)
     return "temp_image.jpg"
 
-
 def analyze_image_with_openai(image):
     # This function will send a request to OpenAI's GPT-4 Vision API to analyze the image.
     # The 'image' parameter can be a URL or base64-encoded data.
+    # Here we provide a mock-up of the API call with placeholder response handling.
 
     response = client.chat.completions.create(
         model="gpt-4-vision-preview",
@@ -29,7 +27,7 @@ def analyze_image_with_openai(image):
             {
                 "role": "user",
                 "content": [
-                    {"type": "text", "text": "What’s in this image? Make a list of only the foods you identify in the image, separating them with commas.. Answer in Portuguese"},
+                    {"type": "text", "text": "What’s in this image? Answer in Portuguese"},
                     {"type": "image_url", "image_url": image},
                 ],
             }
@@ -38,16 +36,9 @@ def analyze_image_with_openai(image):
     )
     description_content = response.choices[0].message.content
     print(description_content)
-   
-      
+    
     return description_content
 
-# Carregar os dados do arquivo CSV
-
-df = pd.read_csv('tabelacal.csv')
-tabela_calorias = df.set_index('Descrição')['Calorias (Kcal)'].to_dict()
-
-# Encontrar o ingrediente mais semelhante
 def encontrar_ingrediente_semelhante(ingrediente, opcoes):
     ingrediente_semelhante, similaridade = process.extractOne(ingrediente, opcoes)
     if similaridade > 60:  # Definir um limiar de similaridade
@@ -67,11 +58,13 @@ def calcular_calorias_do_prato(ingredientes, tabela_calorias):
 
 def main():
     st.image('labcom_logo_preto.jpg')
-    st.title('Calculadora de Calorias de Pratos')
-    st.write('Tenha uma ideia aproximada de quantas calorias você vai ingerir')
+    st.title("Calculadora de Calorias")
+    st.write('Saiba quanto está ingerindo')
     st.write('Use uma imagem de arquivo ou tire uma foto com sua câmera')
-    option = st.radio('Escolha a origem da sua imagem:',('Arquivo', 'Camera'))
+    df = pd.read_csv('tabelacal.csv')
+    tabela_calorias = df.set_index('Descrição')['Calorias (Kcal)'].to_dict()
 
+    option = st.radio('Escolha a origem da sua imagem:',('Arquivo', 'Camera'))
     if option=="Arquivo":
         uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
         if uploaded_file:
@@ -86,10 +79,16 @@ def main():
         
                 # Analyze the image with OpenAI's GPT-4 Vision API
                 description = analyze_image_with_openai(image_url)
-		st.write(description)
+                
+               
+                # Display the description 
+                st.write("Description: ", description)
 
-
-
+		# Calcular calorias
+		total= calcular_calorias_do_prato(description, tabela_calorias)
+		st.write("Total de Calorias: ", total)
+ 
+                
     if option=='Camera':
         picture = st.camera_input("Tire uma foto")
 
@@ -105,14 +104,17 @@ def main():
         
                 # Analyze the image with OpenAI's GPT-4 Vision API
                 description = analyze_image_with_openai(image_url)
-		st.write(description)
-    		
+                
+                # Translate description to Portuguese
+                #translated_description = translate_to_portuguese(description)
+                
+                # Display the description and translated text
+                st.write("Description: ", description)
+                #st.write("Translated Description: ", translated_description)
+                
+                # Convert the description to speech
+                tts(description)
 
-		
-      
-   if __name__ == '__main__':
-	   main()
-
-
-
+if __name__ == '__main__':
+    main()
 
