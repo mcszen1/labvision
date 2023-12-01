8bd376a83beaab984c2a59c8153c3b65443bfb38
mcszen1 <mcszen@gmail.com>
2023-12-01 18:24:17 -0300

    Update Ingredientes.py

--- Ingredientes.py
+++ Ingredientes.py
@@ -126,9 +126,9 @@ def main():
                 # Display the description and translated text
                 st.write("Description: ", description)
 	    if st.button('Calcular Calorias'):
-	    ingredientes_selecionados = description
-    	    total_calorias = calcular_calorias_do_prato(ingredientes_selecionados, tabela_calorias)
-    	    st.write(f"Calorias totais do prato: {total_calorias}")
+		ingredientes_selecionados = description
+	    	total_calorias = calcular_calorias_do_prato(ingredientes_selecionados, tabela_calorias)
+	    	st.write(f"Calorias totais do prato: {total_calorias}")
   
                 
 
