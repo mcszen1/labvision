d2404848b96b35d7db5e0dbb41f657f76ed4de97
mcszen1 <mcszen@gmail.com>
2023-12-17 10:26:26 -0300

    Update LABVisionBeta1.py

--- LABVisionBeta1.py
+++ LABVisionBeta1.py
@@ -52,31 +52,7 @@ def save_uploaded_file(uploaded_file_content):
         f.write(uploaded_file_content)
     return "temp_image.jpg"
 
-def fala(text):
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
 
-    # Instead of using the default speaker, we'll save the audio to a file
-    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
-    audio_config = speechsdk.audio.AudioOutputConfig(filename=temp_file.name)
-
-    # The language of the voice that speaks.
-    speech_config.speech_synthesis_voice_name = 'pt-BR-AntonioNeural'
-
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-
-    speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
-
-    if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-        st.write("Síntese de Voz : ")
-        st.audio(temp_file.name, format='audio/wav')
-    elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_synthesis_result.cancellation_details
-        st.write("Speech synthesis canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            if cancellation_details.error_details:
-                st.write("Error details: {}".format(cancellation_details.error_details))
-                st.write("Did you set the speech resource key and region values?")
 
     # Clean up the temporary file
     os.remove(temp_file.name)
